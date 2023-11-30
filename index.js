caca3444adb51a583ec784941661342f5b1b3478
Mayank Didwania <67261533+Mayank2001-dot@users.noreply.github.com>
2023-11-30 16:26:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ async function fetchDataFromApi(startDate, numberOfMonths) {
   const endDate = new Date(startDate);
   endDate.setMonth(endDate.getMonth() + numberOfMonths);
 
-  const delayBetweenRequests = 2000; // 2000 milliseconds (2 seconds) delay between requests
+  const delayBetweenRequests = 2000; 
 
   const fetchedData = [];
 
@@ -26,26 +26,26 @@ async function fetchDataFromApi(startDate, numberOfMonths) {
       const response = await myPost.json();
       console.log(`Data for ${formattedDate}:`, response);
 
-      // Add fetched data to the array
       fetchedData.push(response);
     } catch (error) {
       console.error(`Error fetching data for ${formattedDate}:`, error.message);
     }
 
-    // Introduce a delay between requests using setTimeout
     await new Promise(resolve => setTimeout(resolve, delayBetweenRequests));
   }
 
   return fetchedData;
 }
 
-async function insertDataIntoDatabase(data) {
-  // Create SQLite database connection
-  const db = new sqlite3.Database('nodedb.db');
+function connectToDatabase(databaseName) {
+  return new sqlite3.Database(databaseName);
+}
 
-  // Create tables (if not already created)
-  db.run(`
-    CREATE TABLE IF NOT EXISTS inventory (
+
+async function createTables(db) {
+
+  await db.run(`
+    CREATE TABLE inventory (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       startDate TEXT,
       startTime TEXT,
@@ -57,8 +57,9 @@ async function insertDataIntoDatabase(data) {
     )
   `);
 
-  db.run(`
-    CREATE TABLE IF NOT EXISTS paxAvailability (
+
+  await db.run(`
+    CREATE TABLE paxAvailability (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       inventoryId INTEGER,
       max INTEGER,
@@ -71,8 +72,8 @@ async function insertDataIntoDatabase(data) {
     )
   `);
 
-  db.run(`
-    CREATE TABLE IF NOT EXISTS price (
+  await db.run(`
+    CREATE TABLE price (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       paxAvailabilityId INTEGER,
       discount REAL,
@@ -81,72 +82,100 @@ async function insertDataIntoDatabase(data) {
       currencyCode TEXT
     )
   `);
+}
 
-  // Insert data into tables
-  db.serialize(() => {
-    const insertInventoryStatement = db.prepare(`
-      INSERT INTO inventory (startDate, startTime, endTime, providerSlotId, remaining, currencyCode, variantId)
-      VALUES (?, ?, ?, ?, ?, ?, ?)
-    `);
 
-    const insertPaxAvailabilityStatement = db.prepare(`
-      INSERT INTO paxAvailability (inventoryId, max, min, remaining, type, isPrimary, description, name)
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-    `);
+async function insertDataIntoTables(db, data) {
 
-    const insertPriceStatement = db.prepare(`
-      INSERT INTO price (paxAvailabilityId, discount, finalPrice, originalPrice, currencyCode)
-      VALUES (?, ?, ?, ?, ?)
-    `);
+  await db.run("BEGIN TRANSACTION");
 
-    for (const inventoryItem of data) {
-      // Insert into 'inventory' table
-      insertInventoryStatement.run(
-        inventoryItem.startDate,
-        inventoryItem.startTime,
-        inventoryItem.endTime,
-        inventoryItem.providerSlotId,
-        inventoryItem.remaining,
-        inventoryItem.currencyCode,
-        inventoryItem.variantId
+  const insertInventoryStatement = db.prepare(`
+    INSERT INTO inventory (startDate, startTime, endTime, providerSlotId, remaining, currencyCode, variantId)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+  `);
+
+  const insertPaxAvailabilityStatement = db.prepare(`
+    INSERT INTO paxAvailability (inventoryId, max, min, remaining, type, isPrimary, description, name)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+  `);
+
+  const insertPriceStatement = db.prepare(`
+    INSERT INTO price (paxAvailabilityId, discount, finalPrice, originalPrice, currencyCode)
+    VALUES (?, ?, ?, ?, ?)
+  `);
+
+  for (const inventoryItem of data) {
+
+    insertInventoryStatement.run(
+      inventoryItem.startDate,
+      inventoryItem.startTime,
+      inventoryItem.endTime,
+      inventoryItem.providerSlotId,
+      inventoryItem.remaining,
+      inventoryItem.currencyCode,
+      inventoryItem.variantId
+    );
+
+    const inventoryId = insertInventoryStatement.lastID;
+
+    for (const paxAvailability of inventoryItem.paxAvailability) {
+
+      insertPaxAvailabilityStatement.run(
+        inventoryId,
+        paxAvailability.max,
+        paxAvailability.min,
+        paxAvailability.remaining,
+        paxAvailability.type,
+        paxAvailability.isPrimary,
+        paxAvailability.description,
+        paxAvailability.name
       );
 
-      const inventoryId = insertInventoryStatement.lastID;
+      const paxAvailabilityId = insertPaxAvailabilityStatement.lastID;
 
-      for (const paxAvailability of inventoryItem.paxAvailability) {
-        // Insert into 'paxAvailability' table
-        insertPaxAvailabilityStatement.run(
-          inventoryId,
-          paxAvailability.max,
-          paxAvailability.min,
-          paxAvailability.remaining,
-          paxAvailability.type,
-          paxAvailability.isPrimary,
-          paxAvailability.description,
-          paxAvailability.name
-        );
 
-        const paxAvailabilityId = insertPaxAvailabilityStatement.lastID;
-
-        // Insert into 'price' table
-        insertPriceStatement.run(
-          paxAvailabilityId,
-          paxAvailability.price.discount,
-          paxAvailability.price.finalPrice,
-          paxAvailability.price.originalPrice,
-          paxAvailability.price.currencyCode
-        );
-      }
+      insertPriceStatement.run(
+        paxAvailabilityId,
+        paxAvailability.price.discount,
+        paxAvailability.price.finalPrice,
+        paxAvailability.price.originalPrice,
+        paxAvailability.price.currencyCode
+      );
     }
+  }
 
-    insertInventoryStatement.finalize();
-    insertPaxAvailabilityStatement.finalize();
-    insertPriceStatement.finalize();
-  });
+  await db.run("COMMIT");
+  insertInventoryStatement.finalize();
+  insertPaxAvailabilityStatement.finalize();
+  insertPriceStatement.finalize();
+}
 
-  // Close the SQLite database connection
+function closeDatabase(db) {
   db.close();
 }
+
+
+async function main() {
+  const databaseName = 'test.db';
+  const startDate = '2023-11-25';
+  const numberOfMonths = 2;
+
+  const db = connectToDatabase(databaseName);
+
+  try {
+
+    await createTables(db);
+
+    const fetchedData = await fetchDataFromApi(startDate, numberOfMonths);
+
+    await insertDataIntoTables(db, fetchedData);
+  } catch (error) {
+    console.error('An error occurred:', error.message);
+  } finally {
+    closeDatabase(db);
+  }
+}
+
 
 function formatDate(date) {
   const year = date.getFullYear();
@@ -155,12 +184,5 @@ function formatDate(date) {
   return `${year}-${month}-${day}`;
 }
 
-async function main() {
-  const startDate = "2023-11-25";
-  const numberOfMonths = 2;
-
-  const fetchedData = await fetchDataFromApi(startDate, numberOfMonths);
-  await insertDataIntoDatabase(fetchedData);
-}
 
 main();
